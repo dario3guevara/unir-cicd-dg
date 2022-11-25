635e367bee1b9f899e400f487c247c8ccb07f1f2
dario3guevara <115838069+dario3guevara@users.noreply.github.com>
2022-11-25 17:17:12 -0500

    Update Jenkinsfile.unit.groovy

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -3,22 +3,11 @@ pipeline {
         label 'docker'
     }
     stages {
-        //FASE UTILIZADA PARA PRE CONSTRUIR EL CORREO
-        stage('PreBuild-Email') {
-           steps {
-               script {
-                   def mailRecipients = 'dario_guevara@hotmail.com'
-                   def jobName = currentBuild.fullDisplayName
-                   //emailext body: '''${SCRIPT, template="groovy-html.template"}''',
-                   emailext body: '''SENIOR USUARIO''',
-                       mimeTye: 'text/html',
-                       subject: "[Jenkins] ha empezado ${jobName}",
-                       to: "${mailRecipients}",
-                       replyTo: "${mailRecipients}",
-                       recipientProviders: [[$class: 'CulpritsRecipientProvider']]
-                    }
-                    }
-                }
+        stage('Source') {
+            steps {
+                git 'https://github.com/srayuso/unir-cicd.git'
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building stage!'
@@ -31,35 +20,27 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('API tests') {
+        stage('Unit API') {
             steps {
                 sh 'make test-api'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('EndtoEnd tests') {
+        stage('Unit Test e2e') {
             steps {
                 sh 'make test-e2e'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-
     }
     post {
         always {
             junit 'results/*_result.xml'
+            echo 'ENVIO DE CORREO ELECTRONICO'
+            // emailext body: 'Numero de Ejecucion: (${BUILD_NUMBER}) - Nombre del Trabajo: '${JOB_NAME}'', subject: 'Correo Electronico', to: 'pedro@pedro.com'
+            echo "Numero de Ejecucion: (${BUILD_NUMBER}) - Nombre del Trabajo: '${JOB_NAME}'"
+            echo "Job '${JOB_NAME}' (${BUILD_NUMBER}) is waiting for input"
+            cleanWs()
         }
-        success {
-            /*SE VALIDA EN EL PIPELINE SON POSITIVAS
-            el nombre de las variables (nombre del proyecto, número de construccíon y URL de construcción)
-            son verificales con el estado existoso del pipeline*/
-            echo   "emailext body: Compruebe la salida de la consola en:${env.BUILD_URL} para ver los resultados \n para: dario_guevara@hotmail.com \n asunto: La construcción falló en Jenkins: ${currentBuild.fullDisplayName}"
-        }
-        /*PIPELINE FALLE*/
-        /*failure {
-            emailext body: 'Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}', 
-                    to: "${EMAIL_TO}", 
-                    subject: 'La Construcción Fallo en  Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'
-        }*/
     }
 }
